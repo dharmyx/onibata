92e51c90b63ec2af5d755b549426272318fbfb36
dharmyx <teminidami@yahoo.co.uk>
2020-06-29 00:10:13 +0100

    File Update
    - Fully Responsive

--- map.js
+++ map.js
@@ -6,3 +6,26 @@ function initMap() {
   });
 }
 
+/*div.logo-footer {
+    width: 100px;
+    height: 80px;
+    margin: 5px auto;
+    background-image: url(Assests/OnibataLogo.png);
+    background-repeat: no-repeat;
+    background-size: 80%;
+    background-position: center;
+}
+div.rights {
+    grid-area: m;
+    margin:  10px auto;
+    font-size: .75rem;
+    font-family: 'Poppins', sans-serif;
+}
+div.rights p a {
+    text-decoration: none;
+    color: #2c0f0b;
+}
+div.socials {
+    grid-area: n;
+    margin: 10px auto;
+}*/
